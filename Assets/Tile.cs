b13f1c220ca77d0214e0bce991aedd40a8a3501f
Por_Que <damianklabuhn@gmail.com>
2020-02-27 21:12:36 +0100

    Tile script + obstacle detection

---
+++ Assets/Tile.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public float hoverAmount; //wartość zwiększania rozmiaru tila
+
+    public LayerMask obstacleLayer;
+
+    private void OnMouseEnter()
+    {
+        transform.localScale += Vector3.one * hoverAmount; //zwiększa rozmiar tila po najechaniu myszką
+    }
+    private void OnMouseExit()
+    {
+        transform.localScale -= Vector3.one * hoverAmount; //zmniejsza rozmiar tila po wyjściu myszki 
+    }
+
+    public bool IsCleat()
+    {
+        Collider2D obstacle = Physics2D.OverlapCircle(transform.position, 0.2f, obstacleLayer); //tworzy niewielkie kółko, które sprawdza czy koliduje z obstacle
+        if (obstacle != null) //zwraca info, czy ma obstacle, czy nie
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+}
