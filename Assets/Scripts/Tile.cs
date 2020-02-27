e201eec93da086061ec82e873257fca549029aed
Por_Que <damianklabuhn@gmail.com>
2020-02-28 00:03:28 +0100

    Selecting Units + walkable tiles highlited

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -4,9 +4,25 @@ using UnityEngine;
 
 public class Tile : MonoBehaviour
 {
+    private SpriteRenderer rend;
+    public Sprite[] tileGraphics; //zbiera sprity tilów
+
     public float hoverAmount; //wartość zwiększania rozmiaru tila
 
     public LayerMask obstacleLayer;
+
+    public Color HighlightedColor;
+    public bool isWalkable;
+    GameMaster gm;
+
+    private void Start()
+    {
+        rend = GetComponent<SpriteRenderer>();
+        int randTile = Random.Range(0, tileGraphics.Length); //losuje sprite tila
+        rend.sprite = tileGraphics[randTile]; //renderuje losowy tile
+
+        gm = FindObjectOfType<GameMaster>();
+    }
 
     private void OnMouseEnter()
     {
@@ -17,7 +33,7 @@ public class Tile : MonoBehaviour
         transform.localScale -= Vector3.one * hoverAmount; //zmniejsza rozmiar tila po wyjściu myszki 
     }
 
-    public bool IsCleat()
+    public bool IsClear()
     {
         Collider2D obstacle = Physics2D.OverlapCircle(transform.position, 0.2f, obstacleLayer); //tworzy niewielkie kółko, które sprawdza czy koliduje z obstacle
         if (obstacle != null) //zwraca info, czy ma obstacle, czy nie
@@ -28,5 +44,17 @@ public class Tile : MonoBehaviour
         {
             return true;
         }
+    }
+
+    public void Highlights()
+    {
+        rend.color = HighlightedColor;
+        isWalkable = true;
+    }
+
+    public void Reset()
+    {
+        rend.color = Color.white;
+        isWalkable = false;
     }
 }
---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    public bool selected; //sprawdza czy ta jednostka jest wybrana
+    GameMaster gm; //daje dostęp do skryptu GameMastera
+
+    public int tileSpeed; //ile tilesów ta jednostka może się poruszyć
+    public bool hasMoved; //sprawdza czy ta jednostka poruszyła się już w tej turze
+
+    private void Start()
+    {
+        gm = FindObjectOfType<GameMaster>();
+    }
+
+    private void OnMouseDown() //po kliknięciu na jednostkę
+    {
+        if (selected == true) //jeżeli jednostka jest już wybrana - odwybiera ją
+        {
+            selected = false;
+            gm.selectedUnit = null;
+        }
+        else //jeżeli ta jednostka nie jest wybrana
+        {
+            if (gm.selectedUnit != null) //inna jednostka jest już wybrana - odwybiera ją
+            {
+                gm.selectedUnit.selected = false;
+            }
+
+            selected = true; //wybiera tą jednostkę
+            gm.selectedUnit = this;
+            GetWalkableTiles();
+        }
+    }
+    void GetWalkableTiles() //pokazuje tile na, które ta jednostka może się poruszyć
+    {
+        if (hasMoved == true) //jeżeli ta jednostka już wcześniej się poruszyła - wyjście  funkcji
+        {
+            return;
+        }
+
+        foreach (Tile tile in FindObjectsOfType<Tile>())
+        {
+            if (Mathf.Abs(transform.position.x - tile.transform.position.x) + Mathf.Abs(transform.position.y - tile.transform.position.y) <= tileSpeed)
+            {
+                if(tile.IsClear() == true)
+                {
+                    tile.Highlights();
+                }
+
+            }
+        }
+    }
+}
