abae7ce3151662a948952e48dc683ac2492a5d4c
Por_Que <damianklabuhn@gmail.com>
2020-03-02 19:03:52 +0100

    2nd player unit added + GameMaster scipt updated: ending turn and swapping players

--- Assets/Scripts/GameMaster.cs
+++ Assets/Scripts/GameMaster.cs
@@ -6,11 +6,44 @@ public class GameMaster : MonoBehaviour
 {
     public Unit selectedUnit; //zapisuje wybraną jednostkę
 
-    public void ResetTiles()
+    public int playerTurn = 1; //zapisuje który gracz ma teraz ture: 1 lewo, 2 prawo
+    public void ResetTiles() //resetuje podświetlenie tilów
     {
         foreach (Tile tile in FindObjectsOfType<Tile>())
         {
             tile.Reset();
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) //jeżeli gracz nadusi wybrany przycisk = kończy swoją ture
+        {
+            EndTurn();
+        }
+    }
+
+    void EndTurn() //zmienia aktywnego gracza
+    {
+        if (playerTurn == 1)
+        {
+            playerTurn = 2;
+        }else if (playerTurn == 2)
+        {
+            playerTurn = 1;
+        }
+
+        if (selectedUnit != null) //gdyby poprzedni gracz zostawił jakąś jednostkę zaznaczoną - odznacza ją
+        {
+            selectedUnit.selected = false;
+            selectedUnit = null;
+        }
+
+        ResetTiles();
+
+        foreach (Unit unit in FindObjectsOfType<Unit>()) //resetuje "zużycie" jednostek
+        {
+            unit.hasMoved = false;
         }
     }
 }
