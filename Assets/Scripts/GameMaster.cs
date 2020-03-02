72a6c41d7c5377d6f77fe498d01a97ff57019561
Por_Que <damianklabuhn@gmail.com>
2020-03-02 20:12:58 +0100

    added custom cursor, unit hover effect and unit highlight when selected

--- Assets/Scripts/GameMaster.cs
+++ Assets/Scripts/GameMaster.cs
@@ -7,6 +7,9 @@ public class GameMaster : MonoBehaviour
     public Unit selectedUnit; //zapisuje wybraną jednostkę
 
     public int playerTurn = 1; //zapisuje który gracz ma teraz ture: 1 lewo, 2 prawo
+
+    public GameObject selectedUnitHighlight; //miejsce na podświetlenie wybranej jednostki
+
     public void ResetTiles() //resetuje podświetlenie tilów
     {
         foreach (Tile tile in FindObjectsOfType<Tile>())
@@ -20,6 +23,16 @@ public class GameMaster : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space)) //jeżeli gracz nadusi wybrany przycisk = kończy swoją ture
         {
             EndTurn();
+        }
+
+        if (selectedUnit != null) //po wybraniu jednostki aktywuje selectedUnitHighlight i porusza nim za jednostką
+        {
+            selectedUnitHighlight.SetActive(true);
+            selectedUnitHighlight.transform.position = selectedUnit.transform.position;
+        }
+        else
+        {
+            selectedUnitHighlight.SetActive(false);
         }
     }
 
---
+++ Assets/Scripts/HoverEffect.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoverEffect : MonoBehaviour
+{
+    public float hoverAmount;
+    private void OnMouseEnter()
+    {
+        transform.localScale += Vector3.one * hoverAmount; //zwiększa rozmiar tila po najechaniu myszką
+    }
+    private void OnMouseExit()
+    {
+        transform.localScale -= Vector3.one * hoverAmount; //zmniejsza rozmiar tila po wyjściu myszki 
+    }
+}
